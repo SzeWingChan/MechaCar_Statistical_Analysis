d15dcf60e7022fd9dbfbb8d7a9389640b5b3a233
Sze-wing Chan <swing.csw@gmail.com>
2022-06-12 21:40:19 -0400

    Deliverable2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-#Deliverable 1
+#Deliverable 1: Linear Regression to Predict MPG
 
 #Load the dplyr package
 library(dplyr)
@@ -9,8 +9,21 @@ MechaCar_Table <-  read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stri
 #Perform multiple linear regression
 fit <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_Table)
 
-#Determine the p-value and the r-squared value 
+#Determine the p-value and the r-squared value using the summary function
 summary(fit)
+
+
+#Deliverable 2: Create Visualizations for the Trip Analysis
+
+#Import and read in the Suspension_Coil.csv file as a dataframe
+SC_Table <-  read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Creates a total_summary dataframe using the summarize() function
+total_summary <- SC_Table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Creates a lot_summary dataframe using the group_by() and the summarize() functions
+lot_summary <- SC_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
 
 
 
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # MechaCar_Statistical_Analysis
 
 ## Linear Regression to Predict MPG
+![Deliverable1](https://github.com/SzeWingChan/MechaCar_Statistical_Analysis/blob/main/Resources/Deliverable1_Results.png)
+
+- Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+  Vehicle length and ground clearance are the variables that have a significant impact on mpg values as their p-values are less than 0.001.  
+
+- Is the slope of the linear model considered to be zero? Why or why not?
+  The slope of the linear model is not zero.  It is because the p-value of the multiple linear model is less than 0.01.
+
+- Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+  The linear model predicts mpg of MechaCar prototypes effectively.  About 71.5% of the variations, denoted by the multiple R-squared, in the data cam be explained by the linear model.
 
